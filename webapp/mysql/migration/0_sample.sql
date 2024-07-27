3c2b8c3b0a884374a4b3f4db42da818705c3c60b
punchlemon <tsukiwahigashini0410@gmail.com>
2024-07-27 17:39:17 +0900

    add index

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,1 +1,8 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
+ALTER TABLE areas ADD INDEX idx(id);
+ALTER TABLE dispatchers ADD INDEX idx(id);
+ALTER TABLE edges ADD INDEX idx(id);
+ALTER TABLE nodes ADD INDEX idx(id);
+ALTER TABLE users ADD INDEX idx(id);
+ALTER TABLE sessions ADD INDEX idx(id);
+ALTER TABLE tow_trucks ADD INDEX idx(id);
